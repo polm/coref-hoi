7eb41ccdfa6d77e2160e94bc2f41d1d1e4612f18
Paul O'Leary McCann <polm@dampfkraft.com>
2021-04-13 15:28:01 +0900

    Read raw conll files to gold data

---
+++ spacy_preprocess.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3 
+# Convert conll annotations to Examples
+# Coref uses a special CoNLL format, so the usual spaCy converters don't work.
+
+import sys
+import re
+from collections import defaultdict
+
+import spacy
+from spacy.tokens import Doc
+from icecream import ic
+
+DOCID_REGEX = "#begin document \((.*)\); part (\d*)"
+
+def convert_conll_doc_to_example(cdoc):
+    pass
+
+def read_file(fname):
+    with open(fname) as infile:
+        text = infile.read()
+
+    nlp = spacy.blank("en")
+
+    
+    docs = text.split("\n#end document\n")
+    for doc in docs:
+        name = None
+        sents = doc.split("\n\n")
+        clustermap = defaultdict(list)
+
+        words = []
+        sent_starts = []
+        for sent in sents:
+            lines = sent.split("\n")
+            if name is None:
+                top = lines.pop(0)
+                # doc id line looks like this:
+                #begin document (/some/path); part 000
+                matches = re.match(DOCID_REGEX, top)
+
+                name = f"{matches.group(1)}_{matches.group(2)}"
+
+            for line in lines:
+                if not line: continue # ignore blanks
+                # note: file is not tsv, it uses ~aligned spaces~
+                fields = line.split()
+                surface = fields[3]
+                # weird escapes
+                if surface in ("/.", "/?"):
+                    surface = surface[1:]
+
+                sent_starts.append(int(fields[2]) == 0)
+                words.append(surface)
+
+                clusters = fields[-1]
+                if clusters == "-":
+                    clusters = []
+                else:
+
+                    # this has to be sorted because the same cluster can be
+                    # annotated twice on the same token, and a cluster can
+                    # contain itself.
+                    # Example: "He himself..."
+                    #   He       (30
+                    #   himself  (30)|30)
+                    clusters = sorted(clusters.split("|"), reverse=True)
+
+                    tokid = len(words) - 1
+                    for mention in clusters:
+                        if mention[0] == "(" and mention[-1] == ")":
+                            cid = int(mention[1:-1])
+                            clustermap[cid].insert(0, (tokid, tokid + 1 ) )
+                        elif mention[0] == "(":
+                            cid = int(mention[1:])
+                            clustermap[cid].append( tokid ) # this will be popped
+                        elif mention[-1] == ")":
+                            cid = int(mention[:-1])
+                            start = clustermap[cid].pop()
+                            clustermap[cid].insert(0, (start, tokid + 1 ) )
+        doc = Doc(nlp.vocab, words=words, sent_starts=sent_starts)
+        for key, vals in clustermap.items():
+            spans = [doc[ss:ee].text for ss, ee in vals]
+            print(key)
+            for val in vals:
+                print("    ", doc[val[0]: val[1]])
+
+if __name__ == "__main__":
+    read_file(sys.argv[1])
